eb202d07dbfa484a86182ea94d9f9e57af070004
pritapuja <prita.pujaastuti@gmail.com>
2024-10-08 13:39:51 +0700

    feat: initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,11 +56,11 @@ function Navbar() {
                 </Link>
               </li>
               <li className="nav-item">
-                <Link
+                <Link href="/covid-19"
                   className={`nav-link ${
                     location.pathname === "/covid-19" ? "active fw-semibold" : "text-muted"
                   }`}
-                  to="/covid-19"
+                  // to="/covid-19"
                 >
                   COVID-19
                 </Link>
